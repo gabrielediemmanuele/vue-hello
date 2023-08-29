725ff98e89dcd3d6b2e3d1de86a3aad00c11954a
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-08-29 14:58:04 +0200

    feat: add html, js + vuejs link + h1 & bonus

---
+++ main.js
@@ -0,0 +1,12 @@
+//* Instanza Vue JS
+
+const { createApp } = Vue;
+createApp({
+  data() {
+    return {
+      message: "Hello Tutors!",
+      newImage:
+        "1091005-fantasy-art-space-Earth-moonlight-universe-meditation-screenshot-computer-wallpaper-outer-space-astronomical-object.jpg",
+    };
+  },
+}).mount("#app");
